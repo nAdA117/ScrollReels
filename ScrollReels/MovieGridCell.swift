64129b48a93f44914cb7798ed341926948cf6bd4
Mitchell <mitchczaja@live.com>
2020-02-06 21:33:35 -0800

    Finished "Superhero" section

---
+++ ScrollReels/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  ScrollReels
+//
+//  Created by MITCHELL CZAJA on 2/6/20.
+//  Copyright © 2020 College of Southern Nevada. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
